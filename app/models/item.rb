396a735d2bed531186cc549ca2fb2f2fc2c4c33c
Josh Cheek <josh.cheek@gmail.com>
2012-02-10 15:52:39 -0600

    Items have images

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ActiveRecord::Base
-  attr_accessible :image
+  attr_accessible :image, :name, :one_hundred, :item_notes, :shopping_suggestions, :tags_attributes  
+  
   validates :name,  :presence => true
   
   has_many :tags
   
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
-  :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
   
   mount_uploader :image, ImageUploader
 end
